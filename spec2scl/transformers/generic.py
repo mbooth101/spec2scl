e3b6b6e9594ad3014e9ad06d84adb96f2e867f90
Mat Booth <mat.booth@redhat.com>
2018-08-21 19:59:43 +0100

    Allow some dep transformations

--- spec2scl/transformers/generic.py
+++ spec2scl/transformers/generic.py
@@ -33,6 +33,20 @@ class GenericTransformer(transformer.Transformer):
         def handle_one_dep(matchobj):
             groupdict = matchobj.groupdict('')
 
+            # Handle any transformations first
+            transformations = {
+                    'mvn(com.google.guava:guava:15.0)':'mvn(com.google.guava:guava)',
+                    'mvn(com.google.guava:guava:16.0)':'mvn(com.google.guava:guava)',
+                    'mvn(com.google.guava:guava:17.0)':'mvn(com.google.guava:guava)',
+                    'mvn(com.google.guava:guava:18.0)':'mvn(com.google.guava:guava)',
+                    'mvn(com.google.guava:guava:19.0)':'mvn(com.google.guava:guava)',
+                    'mvn(com.google.guava:guava:20.0)':'mvn(com.google.guava:guava)',
+                    'mvn(com.google.guava:guava:21.0)':'mvn(com.google.guava:guava)',
+                    'guava20':'guava',
+                    }
+            if groupdict['dep'] in transformations:
+                groupdict['dep'] = transformations[groupdict['dep']]
+
             scl_deps = self.options['scl_deps']
 
             if scl_deps is True or (scl_deps_effect and scl_deps and groupdict['dep'] in scl_deps):
